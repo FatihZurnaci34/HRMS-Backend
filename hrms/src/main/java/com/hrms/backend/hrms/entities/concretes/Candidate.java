2648b3f510825f26c971932fd20fd8cb0ea4068b
gorkem <fatihzurnaci34@gmail.com>
2022-07-21 19:59:54 +0300

    commit

---
+++ hrms/src/main/java/com/hrms/backend/hrms/entities/concretes/Candidate.java
@@ -0,0 +1,32 @@
+package com.hrms.backend.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="candidates")
+public class Candidate {
+	@Id
+	@Column(name="user_id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="nationality_number")
+	private String NationalityNumber;
+	
+
+}
---
+++ hrms/src/main/java/com/hrms/backend/hrms/entities/concretes/Person.java
@@ -0,0 +1,30 @@
+package com.hrms.backend.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="persons")
+public class Person {
+	@Id
+	@Column(name="user_id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	
+
+}
